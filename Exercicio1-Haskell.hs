21e6a0ce98c19ecee8871669f6f3f61d86bcb262
Alex Carvalho <jac@cin.ufpe.br>
2024-04-23 21:41:12 -0300

    Add files via upload

--- Exercicio1-Haskell.hs
+++ Exercicio1-Haskell.hs
@@ -25,6 +25,17 @@ primo x = not (ehDiv x (x-1))
 
 -- Questao 3 -- 
 
+temDiv :: Int -> Int -> Int -> Bool
+temDiv x y d | d == 1 = True
+             | ((mod x d) == 0) && ((mod y d) == 0) = False
+             | otherwise = temDiv x y (d-1)
+
+
+primosEntreSi :: Int -> Int -> Bool
+primosEntreSi x y | x == y = False
+                  | temDiv x y (max x y) = True
+                  | otherwise = False
+
 -- Questao 4 a --
 
 fat :: Int -> Int 
@@ -37,10 +48,18 @@ fat x = x * fat (x-1)
 all4Equal :: Int -> Int -> Int -> Int -> Bool
 all4Equal m n o p = (m == n) && (n == o) && (o == p)
 
-
 -- Questao 4 c --
+
+allEqual :: Int -> Int -> Int -> Bool
+allEqual a b c = (a == b) && (b == c)
+
+all4Equal2 :: Int -> Int -> Int -> Int -> Bool
+all4Equal2 a b c d = (allEqual a b c) && (allEqual b c d)
 
 -- Questao 4 d -- 
 
-
+equalCount :: Int -> Int -> Int -> Int 
+equalCount a b c | allEqual a b c = 3
+                 | (a /= b) && (b /= c) && (c /= a) = 1
+                 | otherwise = 2
 
